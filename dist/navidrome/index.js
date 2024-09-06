6e43203e0d13189311e1e684d52365ee54106d8b
猪小乐 <zhuxiaole@zhuxiaole.org>
2024-09-06 21:56:50 +0800

    first commit

---
+++ dist/navidrome/index.js
@@ -0,0 +1,103 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = require("axios");
+const CryptoJs = require("crypto-js");
+const pageSize = 25;
+const PLUGIN_NAME = "Navidrome";
+const PLUGIN_VERSION = "0.0.1";
+function genSalt() {
+    return Math.random().toString(16).slice(2);
+}
+function getRequestURL(urlPath) {
+    var _a;
+    const userVariables = (_a = env === null || env === void 0 ? void 0 : env.getUserVariables()) !== null && _a !== void 0 ? _a : {};
+    let { url, username, password } = userVariables;
+    if (!(url && username && password)) {
+        return null;
+    }
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = `http://${url}`;
+    }
+    const salt = genSalt();
+    const urlObj = new URL(`${url}/rest/${urlPath}`);
+    urlObj.searchParams.append("u", username);
+    urlObj.searchParams.append("s", salt);
+    urlObj.searchParams.append("t", CryptoJs.MD5(`${password}${salt}`).toString(CryptoJs.enc.Hex));
+    urlObj.searchParams.append("c", `"MusicFree-Pig"${PLUGIN_NAME}`);
+    urlObj.searchParams.append("v", PLUGIN_VERSION);
+    urlObj.searchParams.append("f", "json");
+    return urlObj;
+}
+async function httpGet(urlPath, params) {
+    return (await axios_1.default.get(getRequestURL(urlPath).toString(), {
+        params: Object.assign({}, params),
+    })).data;
+}
+function formatMusicItem(it) {
+    return Object.assign(Object.assign({}, it), { artwork: it.coverArt });
+}
+function formatAlbumItem(it) {
+    return Object.assign(Object.assign({}, it), { artwork: it.coverArt });
+}
+async function searchMusic(query, page) {
+    const data = await httpGet("search2", {
+        query,
+        songCount: pageSize,
+        songOffset: (page - 1) * pageSize,
+    });
+    const songs = data["subsonic-response"].searchResult2.song;
+    return {
+        isEnd: songs.length < pageSize,
+        data: songs.map(formatMusicItem),
+    };
+}
+async function searchAlbum(query, page) {
+    const data = await httpGet("search2", {
+        query,
+        albumCount: pageSize,
+        albumOffset: (page - 1) * pageSize,
+    });
+    const songs = data["subsonic-response"].searchResult2.album;
+    return {
+        isEnd: songs.length < pageSize,
+        data: songs.map(formatAlbumItem),
+    };
+}
+module.exports = {
+    platform: `${PLUGIN_NAME}`,
+    version: `${PLUGIN_VERSION}`,
+    author: "猪小乐",
+    appVersion: ">0.1.0-alpha.0",
+    srcUrl: "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/navidrome/index.js",
+    cacheControl: "no-cache",
+    userVariables: [
+        {
+            key: "url",
+            name: "服务器地址",
+        },
+        {
+            key: "username",
+            name: "用户名",
+        },
+        {
+            key: "password",
+            name: "密码",
+        },
+    ],
+    supportedSearchType: ["music", "album"],
+    async search(query, page, type) {
+        if (type === "music") {
+            return await searchMusic(query, page);
+        }
+        if (type === "album") {
+            return await searchAlbum(query, page);
+        }
+    },
+    async getMediaSource(musicItem) {
+        const urlObj = getRequestURL("stream");
+        urlObj.searchParams.append("id", musicItem.id);
+        return {
+            url: urlObj.toString(),
+        };
+    },
+};
