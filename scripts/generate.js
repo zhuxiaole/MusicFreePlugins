a69dabe8c7265e18f4e4422730640de27ac10bfd
猪小乐 <zhuxiaole@zhuxiaole.org>
2024-09-09 20:52:44 +0800

    修改生成plugins.json的路径

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,44 +1,68 @@
 const fs = require('fs/promises');
 const path = require('path');
-const rimraf = require('rimraf')
+const rimraf = require('rimraf');
 
-const basePath = path.resolve(__dirname, '../dist');
+const basePath = path.resolve(__dirname, '..');
+const distPath = path.resolve(__dirname, '../dist');
 
 async function run() {
     console.log('生成json文件...');
-    const pluginPath = path.resolve(basePath, '_plugins');
+
+    const pluginPath = path.resolve(distPath, 'plugins.json');
+    const mypluginsPath = path.resolve(distPath, 'myplugins.json');
+    const distPackagePath = path.resolve(distPath, 'package.json')
+
     await rimraf(pluginPath);
-    await fs.mkdir(pluginPath);
-    const bundledPlugins = await fs.readdir(basePath);
+    await rimraf(mypluginsPath);
+    await rimraf(distPackagePath);
+
+    const bundledPlugins = await fs.readdir(distPath);
+
     const output = {
         plugins: []
     };
-    await Promise.all(bundledPlugins.map(async (bundleFolder) => {
-        if (!bundleFolder.startsWith('_')) {
-            try {
-                const targetPluginPath = path.resolve(basePath, bundleFolder, 'index.js');
-                await fs.stat(targetPluginPath);
-                const origin = await fs.readFile(targetPluginPath, 'utf-8');
-                const mexports = origin.match(/module.exports\s*=\s*([\s\S]*)$/)[1];
-                const platform = mexports.match(/platform:\s*['"`](.*)['"`]/)[1]
-                const version = mexports.match(/version:\s*['"`](.*)['"`]/)[1]
-                const srcUrl = mexports.match(/srcUrl:\s*['"`](.*)['"`]/)[1]
+
+    await Promise.all(bundledPlugins.map(async (file) => {
+        const filePath = path.resolve(distPath, file);
+        const stat = await fs.stat(filePath);
+
+        if (stat.isDirectory()) {
+            const targetPluginPath = path.resolve(filePath, 'index.js');
+            await fs.stat(targetPluginPath);
+            const origin = await fs.readFile(targetPluginPath, 'utf-8');
+            const mexportsMatch = origin.match(/module.exports\s*=\s*([\s\S]*)$/);
+
+            if (mexportsMatch) {
+                const mexports = mexportsMatch[1];
+                const platform = mexports.match(/platform:\s*['"`](.*)['"`]/)[1];
+                const version = mexports.match(/version:\s*['"`](.*)['"`]/)[1];
+                const srcUrl = mexports.match(/srcUrl:\s*['"`](.*)['"`]/)[1];
 
                 output.plugins.push({
                     name: platform,
                     url: srcUrl,
                     version: version
-                })
-            } catch(e) {
-                console.warn('异常:', e);
-             }
+                });
+            }
         }
-    }))
+    }));
 
-    await fs.writeFile(path.resolve(pluginPath, 'plugins.json'), JSON.stringify(output));
-    await fs.copyFile(path.resolve(pluginPath, 'plugins.json'), path.resolve(__dirname, '../plugins.json'))
+    // 写入 plugins.json
+    await fs.writeFile(pluginPath, JSON.stringify(output,"","  "));
+
+    // 写入 myplugins.json
+    const mypluginBasePath = path.resolve(basePath, 'myplugins-base.json');
+    await fs.stat(mypluginBasePath);
+    const mypluginsBase = await fs.readFile(mypluginBasePath, 'utf-8');
+    const mypluginsBaseJson = JSON.parse(mypluginsBase);
+    mypluginsBaseJson.plugins.push(...output.plugins);
+
+    await fs.writeFile(mypluginsPath, JSON.stringify(mypluginsBaseJson,"","  "));
+
+    // 复制 publish-package.json 至 dist/package.json
+    await fs.copyFile(path.resolve(basePath, 'publish-package.json'), distPackagePath);
+
     console.log('done√');
-
 }
 
 
